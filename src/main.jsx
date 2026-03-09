4d0b34f53489a13ca8c887b32055319af970a08d
ck <jchangkuan@gmail.com>
2026-03-09 16:02:08 +0800

    config: inject real Google Client ID

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 // In a real production app, this would be an environment variable
-const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID_HERE"
+const GOOGLE_CLIENT_ID = "465130782612-o8h75ukhbs5vdkh8mhl70q4fdbsvgf7a.apps.googleusercontent.com"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
